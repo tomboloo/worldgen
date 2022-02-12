85001a1cdecc94e981f4b11a0ffeeb9b6f6a3569
William Schroeder <william.schroeder78@gmail.com>
2022-02-12 01:08:27 -0700

    this one is GOATED

--- src/main/java/com/gmail/tommyfarron/worldgen/WastelandBiomeProvider.java
+++ src/main/java/com/gmail/tommyfarron/worldgen/WastelandBiomeProvider.java
@@ -11,7 +11,10 @@ import java.util.List;
 public class WastelandBiomeProvider extends BiomeProvider {
     @Override
     public Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
-        return Biome.ERODED_BADLANDS;
+        if (y > 80) return Biome.BADLANDS;
+        else if (y > 70) return Biome.ERODED_BADLANDS;
+        else if (y > 50) return Biome.DESERT;
+        else return Biome.LUSH_CAVES;
     }
 
     @Override
@@ -20,6 +23,7 @@ public class WastelandBiomeProvider extends BiomeProvider {
         biomes.add(Biome.ERODED_BADLANDS);
         biomes.add(Biome.BADLANDS);
         biomes.add(Biome.DESERT);
+        biomes.add(Biome.LUSH_CAVES);
         return biomes;
     }
 }

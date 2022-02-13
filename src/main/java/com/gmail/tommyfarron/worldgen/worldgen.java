38fc1c957a5fea36c21c23ae24405e02657d88c2
William Schroeder <william.schroeder78@gmail.com>
2022-02-12 19:18:28 -0700

    register event handler

--- src/main/java/com/gmail/tommyfarron/worldgen/worldgen.java
+++ src/main/java/com/gmail/tommyfarron/worldgen/worldgen.java
@@ -1,6 +1,10 @@
 package com.gmail.tommyfarron.worldgen;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Material;
 import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
 import org.bukkit.generator.BiomeProvider;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,14 +12,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class worldgen extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new WastelandEventListener(), this);
 		getLogger().info("worldgen-0.0.6 has loaded successfully");
-		
-		}
-	
+	}
+
 	@Override
 	public void onDisable() {
 		getLogger().info("worldgen-0.0.6 has shut down successfully");
-		
 	}
 
 	@Override
